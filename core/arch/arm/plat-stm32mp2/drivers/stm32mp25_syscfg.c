d56bfc705d209e0e850759018aa45879cad93614
Pascal Paillet <p.paillet@st.com>
2022-04-11 11:35:01 +0200

    plat-stm32mp2: sysconfig: Handle IO compensation
    Add interface to enable and disable IO compensation
    for the five IO domains.
    Low power has to be handled by the user.
    Add nex property st,iocomp to set a fixed IO compensation
    or to use the dynamic IO compensation.
    
    Change-Id: I09b32920a4b0424f7c0dfe86e719e2cad2d87d79
    Signed-off-by: Pascal Paillet <p.paillet@st.com>
    Signed-off-by: Christophe Kerello <christophe.kerello@foss.st.com>
    Signed-off-by: Thomas Bourgoin <thomas.bourgoin@foss.st.com>

--- core/arch/arm/plat-stm32mp2/drivers/stm32mp25_syscfg.c
+++ core/arch/arm/plat-stm32mp2/drivers/stm32mp25_syscfg.c
@@ -6,6 +6,7 @@
 #include <assert.h>
 #include <drivers/stm32_shared_io.h>
 #include <io.h>
+#include <kernel/delay.h>
 #include <kernel/panic.h>
 #include <mm/core_memprot.h>
 #include <stm32_sysconf.h>
@@ -39,6 +40,31 @@ struct io_pa_va syscfg_base[SYSCON_NB_BANKS] = {
 	{ .pa = A35SSC_BASE }
 };
 
+/* IO compensation CCR registers bit definition */
+#define SYSCFG_CCCR_CS				BIT(9)
+#define SYSCFG_CCCR_EN				BIT(8)
+#define SYSCFG_CCCR_RAPSRC_MASK			GENMASK_32(7, 4)
+#define SYSCFG_CCCR_RAPSRC_SHIFT		U(4)
+#define SYSCFG_CCCR_RANSRC_MASK			GENMASK_32(3, 0)
+
+/* IO compensation CCSR registers bit definition */
+#define SYSCFG_CCSR_READY			BIT(8)
+#define SYSCFG_CCSR_APSRC_MASK			GENMASK_32(7, 4)
+#define SYSCFG_CCSR_ANSRC_MASK			GENMASK_32(3, 0)
+
+#define SYSCFG_CCSR_READY_TIMEOUT_US		U(1000)
+
+/*
+ * SYSCFG IO compensation register offsets (base relative)
+ */
+static uint32_t syscfg_cccr_offset[SYSFG_NB_IO_ID] = {
+	[SYSFG_VDDIO1_ID] = SYSCON_ID(SYSCON_SYSCFG, 0x4020U),
+	[SYSFG_VDDIO2_ID] = SYSCON_ID(SYSCON_SYSCFG, 0x4018U),
+	[SYSFG_VDDIO3_ID] = SYSCON_ID(SYSCON_SYSCFG, 0x4000U),
+	[SYSFG_VDDIO4_ID] = SYSCON_ID(SYSCON_SYSCFG, 0x4008U),
+	[SYSFG_VDD_IO_ID] = SYSCON_ID(SYSCON_SYSCFG, 0x4010U),
+};
+
 static vaddr_t stm32mp_syscfg_base(uint32_t id)
 {
 	uint32_t bank = SYSCON_BANK(id);
@@ -59,6 +85,77 @@ uint32_t stm32mp_syscfg_read(uint32_t id)
 {
 	return io_read32(stm32mp_syscfg_base(id) + SYSCON_OFFSET(id));
 }
+
+TEE_Result stm32mp25_syscfg_enable_iocomp(enum syscfg_io_ids id)
+{
+	vaddr_t cccr_addr = stm32mp_syscfg_base(SYSCON_SYSCFG) +
+			    SYSCON_OFFSET(syscfg_cccr_offset[id]);
+	vaddr_t ccsr_addr = cccr_addr + 4U;
+	uint64_t timeout_ref = 0;
+
+	FMSG("Enable IO comp for id %u", id);
+
+	if (io_read32(ccsr_addr) & SYSCFG_CCSR_READY)
+		return TEE_SUCCESS;
+
+	io_setbits32(cccr_addr, SYSCFG_CCCR_EN);
+
+	timeout_ref = timeout_init_us(SYSCFG_CCSR_READY_TIMEOUT_US);
+
+	while (!(io_read32(ccsr_addr) & SYSCFG_CCSR_READY))
+		if (timeout_elapsed(timeout_ref))
+			break;
+
+	if (!(io_read32(ccsr_addr) & SYSCFG_CCSR_READY)) {
+		EMSG("IO compensation cell not ready");
+		return TEE_ERROR_GENERIC;
+	}
+
+	io_clrbits32(cccr_addr, SYSCFG_CCCR_CS);
+
+	return TEE_SUCCESS;
+}
+
+TEE_Result stm32mp25_syscfg_disable_iocomp(enum syscfg_io_ids id)
+{
+	vaddr_t cccr_addr = stm32mp_syscfg_base(SYSCON_SYSCFG) +
+			    SYSCON_OFFSET(syscfg_cccr_offset[id]);
+	vaddr_t ccsr_addr = cccr_addr + 4U;
+	uint32_t value = 0;
+
+	FMSG("Disable IO comp for id %u", id);
+
+	if (!(io_read32(ccsr_addr) & SYSCFG_CCSR_READY))
+		return TEE_SUCCESS;
+
+	value = io_read32(ccsr_addr) & (SYSCFG_CCSR_APSRC_MASK |
+		                        SYSCFG_CCSR_ANSRC_MASK);
+
+	io_clrsetbits32(cccr_addr,
+			SYSCFG_CCCR_RAPSRC_MASK | SYSCFG_CCCR_RANSRC_MASK,
+			value);
+
+	io_setbits32(cccr_addr, SYSCFG_CCCR_CS);
+
+	io_clrbits32(cccr_addr, SYSCFG_CCCR_EN);
+
+	return TEE_SUCCESS;
+}
+
+void stm32mp25_syscfg_fixed_iocomp(enum syscfg_io_ids id,
+				   uint32_t pmos, uint32_t nmos)
+{
+	vaddr_t cccr_addr = stm32mp_syscfg_base(SYSCON_SYSCFG) +
+			    SYSCON_OFFSET(syscfg_cccr_offset[id]);
+	uint32_t value = 0;
+
+	FMSG("Fixed IO comp for id %u", id);
+
+	value = ((pmos << SYSCFG_CCCR_RAPSRC_SHIFT) & SYSCFG_CCCR_RAPSRC_MASK) |
+		(nmos & SYSCFG_CCCR_RANSRC_MASK);
+	io_write32(cccr_addr, value);
+}
+
 
 void stm32mp25_syscfg_set_safe_reset(bool status)
 {
