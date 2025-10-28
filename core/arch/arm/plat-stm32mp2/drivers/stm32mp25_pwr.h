4e31844407ff79cb4cf53e3fb0d2ac9ef180a9aa
Thomas Bourgoin <thomas.bourgoin@foss.st.com>
2025-10-28 15:24:30 +0100

    plat-stm32mp2: add PWR driver for STM32MP2 family
    Implement PWR driver for stm32mp21 and stm32mp25.
    
    Signed-off-by: Pascal Paillet <p.paillet@foss.st.com>
    Signed-off-by: Thomas Bourgoin <thomas.bourgoin@foss.st.com>

---
+++ core/arch/arm/plat-stm32mp2/drivers/stm32mp25_pwr.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: BSD-3-Clause */
+/*
+ * Copyright (c) 2022, STMicroelectronics
+ */
+
+#ifndef __STM32MP25_PWR_H
+#define __STM32MP25_PWR_H
+
+#include <kernel/interrupt.h>
+#include <tee_api_types.h>
+#include <types_ext.h>
+#include <util.h>
+
+enum pwr_monitoring {
+	PWR_MON_V08CAP_TEMP,
+	PWR_MON_VCORE,
+	PWR_MON_VCPU,
+};
+
+vaddr_t stm32_pwr_base(void);
+
+#endif /*__STM32MP25_PWR_H*/
