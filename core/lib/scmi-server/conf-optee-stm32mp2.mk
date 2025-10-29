c33e4b800a5c86fb38e772358237de220bd165cc
Thomas Bourgoin <thomas.bourgoin@foss.st.com>
2025-10-29 11:54:26 +0100

    plat-stm32mp2: conf: enable SCP SCMI regulator
    Enable module OPTEE_VOLTD_REGULATOR, SCMI_VOLTAGE_DOMAIN and
    VOLTAGE_DOMAIN for stm32mp2 plateform.
    
    Change-Id: I42fd36cf3386dd909f56def2d8e3b2437bdc9a94
    Signed-off-by: Thomas Bourgoin <thomas.bourgoin@foss.st.com>

--- core/lib/scmi-server/conf-optee-stm32mp2.mk
+++ core/lib/scmi-server/conf-optee-stm32mp2.mk
@@ -4,12 +4,16 @@ $(call force,CFG_SCPFW_MOD_OPTEE_CLOCK,y)
 $(call force,CFG_SCPFW_MOD_OPTEE_CONSOLE,y)
 $(call force,CFG_SCPFW_MOD_OPTEE_MBX,y)
 $(call force,CFG_SCPFW_MOD_OPTEE_RESET,y)
+$(call force,CFG_SCPFW_MOD_OPTEE_VOLTD_REGULATOR,y)
 $(call force,CFG_SCPFW_MOD_RESET_DOMAIN,y)
 $(call force,CFG_SCPFW_MOD_SCMI,y)
 $(call force,CFG_SCPFW_MOD_SCMI_CLOCK,y)
 $(call force,CFG_SCPFW_MOD_SCMI_RESET_DOMAIN,y)
+$(call force,CFG_SCPFW_MOD_SCMI_VOLTAGE_DOMAIN,y)
 # Info level is sufficient for scp-firmware
 CFG_SCPFW_LOG_LEVEL ?= 1
+
+$(call force,CFG_SCPFW_MOD_VOLTAGE_DOMAIN,y)
 
 $(call force,CFG_SCPFW_NOTIFICATION,n)
 $(call force,CFG_SCPFW_FAST_CHANNEL,n)
