gradhocJcalvo/optee-graddeck

drivers: stm32_rifsc: provide new API for CID filtering feature
Some drivers may need to disable CID filtering for specific actions.
Add a new API to know if CID filtering is enabled and to
enable/disable it

Signed-off-by: Francois Choleau <francois.choleau@st.com>
Change-Id: I89458ce62786e9b276512c76c54e3036f89b9fdc
Reviewed-on: https://gerrit.st.com/c/mpu/oe/optee/optee_os/+/375005
ACI: CITOOLS <MDG-smet-aci-reviews@list.st.com>
Domain-Review: Gatien CHEVALLIER <gatien.chevallier@st.com>
Reviewed-by: Gatien CHEVALLIER <gatien.chevallier@st.com>
Tested-by: Gatien CHEVALLIER <gatien.chevallier@st.com>

core/drivers/firewall/stm32_rifsc.c
@@ -553,6 +553,34 @@ TEE_Result stm32_rifsc_reconfigure_risup(unsigned int risup_id,
 	return TEE_SUCCESS;
 }
 
+bool stm32_rifsc_cid_is_enabled(unsigned int  rifc_id)
+{
+	struct io_pa_va rifsc_addr = { .pa = RIFSC_BASE };
+	vaddr_t rifsc_base = io_pa_or_va(&rifsc_addr, 1);
+	uint32_t cidcfgr = io_read32(rifsc_base + _RIFSC_RISC_PER0_CIDCFGR +
+				     _OFST_PERX_CIDCFGR * rifc_id);
+
+	return (cidcfgr & _CIDCFGR_CFEN) == _CIDCFGR_CFEN;
+}
+
+void stm32_rifsc_cid_enable(unsigned int  rifc_id)
+{
+	struct io_pa_va rifsc_addr = { .pa = RIFSC_BASE };
+	vaddr_t rifsc_base = io_pa_or_va(&rifsc_addr, 1);
+
+	io_setbits32(rifsc_base + _RIFSC_RISC_PER0_CIDCFGR +
+		     _OFST_PERX_CIDCFGR * rifc_id, _CIDCFGR_CFEN);
+}
+
+void stm32_rifsc_cid_disable(unsigned int  rifc_id)
+{
+	struct io_pa_va rifsc_addr = { .pa = RIFSC_BASE };
+	vaddr_t rifsc_base = io_pa_or_va(&rifsc_addr, 1);
+
+	io_clrbits32(rifsc_base + _RIFSC_RISC_PER0_CIDCFGR +
+		     _OFST_PERX_CIDCFGR * rifc_id, _CIDCFGR_CFEN);
+}
+
 TEE_Result stm32_rifsc_reconfigure_rimu(unsigned int id,
 					unsigned int master_cid,
 					bool cid_sel, bool sec, bool priv)

tbourgoi/optee_os

core/drivers/firewall/stm32_rifsc.c
@@ -731,6 +731,34 @@ static TEE_Result stm32_risal_setup(struct rifsc_platdata *pdata)
 }
 #endif /* CFG_STM32MP25 */
 
+bool stm32_rifsc_cid_is_enabled(unsigned int  rifc_id)
+{
+	struct io_pa_va rifsc_addr = { .pa = RIFSC_BASE };
+	vaddr_t rifsc_base = io_pa_or_va(&rifsc_addr, 1);
+	uint32_t cidcfgr = io_read32(rifsc_base + _RIFSC_RISC_PER0_CIDCFGR +
+				     _OFFSET_PERX_CIDCFGR * rifc_id);
+
+	return (cidcfgr & _CIDCFGR_CFEN) == _CIDCFGR_CFEN;
+}
+
+void stm32_rifsc_cid_enable(unsigned int  rifc_id)
+{
+	struct io_pa_va rifsc_addr = { .pa = RIFSC_BASE };
+	vaddr_t rifsc_base = io_pa_or_va(&rifsc_addr, 1);
+
+	io_setbits32_stm32shregs(rifsc_base + _RIFSC_RISC_PER0_CIDCFGR +
+				 _OFFSET_PERX_CIDCFGR * rifc_id, _CIDCFGR_CFEN);
+}
+
+void stm32_rifsc_cid_disable(unsigned int  rifc_id)
+{
+	struct io_pa_va rifsc_addr = { .pa = RIFSC_BASE };
+	vaddr_t rifsc_base = io_pa_or_va(&rifsc_addr, 1);
+
+	io_clrbits32_stm32shregs(rifsc_base + _RIFSC_RISC_PER0_CIDCFGR +
+				 _OFFSET_PERX_CIDCFGR * rifc_id, _CIDCFGR_CFEN);
+}
+
 static TEE_Result stm32_rifsc_check_access(struct firewall_query *firewall)
 {
 	uintptr_t rifsc_base = rifsc_pdata.base;